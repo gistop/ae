4a1eed6e34cccb20472fd66757edf6309faf06aa
gistop <gistop@163.com>
2016-02-06 06:49:14 +0800

    add

---
+++ ae/CenterForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace ae
+{
+    public partial class CenterForm : DockContent
+    {
+
+        private AttributionForm attributionform = new AttributionForm();
+        private LayoutForm layoutForm = new LayoutForm();
+        private MapForm mapForm = new MapForm();
+
+        
+        public CenterForm()
+        {
+            InitializeComponent();
+
+            //attributionform.Show(dockPanel2);
+            //layoutForm.Show(dockPanel2);
+            //mapForm.Show(dockPanel2);
+        }
+    }
+}
